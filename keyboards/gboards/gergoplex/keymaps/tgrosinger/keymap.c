e969ad8659fe590d7510259093a84c8de3c1c67c
Tony Grosinger <tony@grosinger.net>
2022-04-13 12:32:57 -0700

    Remove problematic hold shortcuts
    Moved shortcuts to new layer where they will be easy to trigger, but
    harder to trigger accidentally.

--- keyboards/gboards/gergoplex/keymaps/tgrosinger/keymap.c
+++ keyboards/gboards/gergoplex/keymaps/tgrosinger/keymap.c
@@ -24,24 +24,30 @@ enum {
   _TMUX,   // TMUX Nav layer
 };
 
+// Keycode aliases
+
+#define ___   KC_NO
+#define LDSK  C(G(KC_LEFT))
+#define RDSK  C(G(KC_RIGHT))
+
 
 // Macros
 enum custom_keycodes {
-  TMUX_NEW = SAFE_RANGE,
-  TMUX_ZOOM,
-  TMUX_SCROLL,
-  TMUX_PN_LT,
-  TMUX_PN_UP,
-  TMUX_PN_DN,
-  TMUX_PN_RT,
-  TMUX_WN_LT,
-  TMUX_WN_RT,
-  TMUX_SP_VT,
-  TMUX_SP_HZ,
-  VIM_CLIP_CP,
-  VIM_CLIP_PST,
-  VIM_MACRO,
-  VIM_SAVE,
+  TX_NEW = SAFE_RANGE,
+  TX_ZOOM,
+  TX_SCROLL,
+  TX_PN_LT,
+  TX_PN_UP,
+  TX_PN_DN,
+  TX_PN_RT,
+  TX_WN_LT,
+  TX_WN_RT,
+  TX_SP_VT,
+  TX_SP_HZ,
+  VI_CLIP_CP,
+  VI_CLIP_PST,
+  VI_MACRO,
+  VI_SAVE,
 };
 
 // Tap Dance declarations
@@ -162,128 +168,93 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case LT(0, KC_H):
-      // Override "h" if held
-      if (!record->tap.count && record->event.pressed) {
-        tap_code16(C(G(KC_LEFT)));
-        return false;
-      }
-      return true;
-    case LT(0, KC_L):
-      // Override "l" if held
-      if (!record->tap.count && record->event.pressed) {
-        tap_code16(C(G(KC_RIGHT)));
-        return false;
-      }
-      return true;
-    case LT(0, KC_Z):
-      // Override "z" if held
-      if (!record->tap.count && record->event.pressed) {
-        tap_code16(C(S(KC_ESC)));
-        return false;
-      }
-      return true;
-    case LT(0, KC_E):
-      // Override "e" if held
-      if (!record->tap.count && record->event.pressed) {
-        tap_code16(G(KC_E));
-        return false;
-      }
-      return true;
-    case LT(0, KC_R):
-      // Override "r" if held
-      if (!record->tap.count && record->event.pressed) {
-        tap_code16(G(KC_R));
-        return false;
-      }
-      return true;
-    case TMUX_NEW:
+    case TX_NEW:
       if (record->event.pressed) {
         tap_code16(C(KC_B));
         SEND_STRING("c");
       }
       break;
-    case TMUX_ZOOM:
+    case TX_ZOOM:
       if (record->event.pressed) {
         tap_code16(C(KC_B));
         SEND_STRING("z");
       }
       break;
-    case TMUX_SCROLL:
+    case TX_SCROLL:
       if (record->event.pressed) {
         tap_code16(C(KC_B));
         SEND_STRING("[");
       }
       break;
-    case TMUX_PN_LT:
+    case TX_PN_LT:
       if (record->event.pressed) {
         tap_code16(C(KC_B));
         tap_code(KC_LEFT);
       }
       break;
-    case TMUX_PN_DN:
+    case TX_PN_DN:
       if (record->event.pressed) {
         tap_code16(C(KC_B));
         tap_code(KC_DOWN);
       }
       break;
-    case TMUX_PN_UP:
+    case TX_PN_UP:
       if (record->event.pressed) {
         tap_code16(C(KC_B));
         tap_code(KC_UP);
       }
       break;
-    case TMUX_PN_RT:
+    case TX_PN_RT:
       if (record->event.pressed) {
         tap_code16(C(KC_B));
         tap_code(KC_RIGHT);
       }
       break;
-    case TMUX_WN_LT:
+    case TX_WN_LT:
       if (record->event.pressed) {
         tap_code16(C(KC_B));
         SEND_STRING("p");
       }
       break;
-    case TMUX_WN_RT:
+    case TX_WN_RT:
       if (record->event.pressed) {
         tap_code16(C(KC_B));
         SEND_STRING("n");
       }
       break;
-    case TMUX_SP_VT:
+    case TX_SP_VT:
       if (record->event.pressed) {
         tap_code16(C(KC_B));
         SEND_STRING("%");
       }
       break;
-    case TMUX_SP_HZ:
+    case TX_SP_HZ:
       if (record->event.pressed) {
         tap_code16(C(KC_B));
         SEND_STRING("\"");
       }
       break;
-    case VIM_CLIP_CP:
+    case VI_CLIP_CP:
       if (record->event.pressed) {
         tap_code16(S(KC_QUOTE));
         tap_code16(S(KC_EQUAL));
         tap_code(KC_Y);
       }
       break;
-    case VIM_CLIP_PST:
+    case VI_CLIP_PST:
       if (record->event.pressed) {
         tap_code16(S(KC_QUOTE));
         tap_code16(S(KC_EQUAL));
         tap_code(KC_P);
       }
       break;
-    case VIM_MACRO:
+    case VI_MACRO:
       if (record->event.pressed) {
         tap_code16(S(KC_2));
         tap_code(KC_Q);
       }
       break;
-    case VIM_SAVE:
+    case VI_SAVE:
       if (record->event.pressed) {
         tap_code(KC_ESC);
         tap_code16(KC_COLN);
@@ -313,30 +284,31 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 // TODO: Add a back and forward button for use in the browser?
+// TODO: Consider demoting comma, slash, and colon to other layers.
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_ALPHA] = LAYOUT_split_3x5_3(
-      KC_Q,        KC_W, LT(0, KC_E), LT(0, KC_R), KC_T,      KC_Y,        KC_U,        KC_I,        KC_O,        KC_P, 
-      CTL_T(KC_A), KC_S, KC_D,        KC_F,        KC_G,      LT(0, KC_H), GUI_T(KC_J), ALT_T(KC_K), LT(0, KC_L), TD(SCLN_COLN),
-      LT(0, KC_Z), KC_X, KC_C,        KC_V,        KC_B,      KC_N,        KC_M,        KC_COMM,     KC_DOT,      TD(SLSH_BSLS),
-          KC_NO, TD(NUMB_TMUX), KC_LSFT,                              KC_SPC, MO(_SYMB), KC_NO
+      KC_Q,        KC_W, KC_E, KC_R, KC_T, /**/ KC_Y,        KC_U,        KC_I,        KC_O,        KC_P, 
+      CTL_T(KC_A), KC_S, KC_D, KC_F, KC_G, /**/ LT(0, KC_H), GUI_T(KC_J), ALT_T(KC_K), LT(0, KC_L), TD(SCLN_COLN),
+      KC_Z,        KC_X, KC_C, KC_V, KC_B, /**/ KC_N,        KC_M,        KC_COMM,     KC_DOT,      TD(SLSH_BSLS),
+      /**/ ___, TD(NUMB_TMUX), KC_LSFT, /*         */ KC_SPC, MO(_SYMB), ___
       ),
   [_NUMB] = LAYOUT_split_3x5_3(
-      KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,  KC_7, KC_8, KC_9,  KC_0,
-      KC_F1,   KC_F2,   KC_F12,  KC_F4,   KC_F5,        KC_NO, KC_4, KC_5, KC_6,  KC_COLN,
-      KC_LSFT, KC_NO,   KC_NO,   KC_NO,   KC_NO,        KC_NO, KC_1, KC_2, KC_3,  KC_DOT,
-          KC_NO, KC_TRNS, DF(_ALPHA),                       DF(_NUMB), KC_0, KC_NO
+      KC_1,  KC_2,  KC_3,    KC_4,    KC_5,  /**/ KC_6,  KC_7, KC_8, KC_9,  KC_0,
+      ___,   LDSK,  G(KC_E), G(KC_R), RDSK,  /**/ ___, KC_4, KC_5, KC_6,  KC_COLN,
+      KC_F1, KC_F2, KC_F12,  KC_F4,   KC_F5, /**/ ___, KC_1, KC_2, KC_3,  KC_DOT,
+      /**/ ___, KC_TRNS, DF(_ALPHA), /*               */ DF(_NUMB), KC_0, ___
       ),
   [_SYMB] = LAYOUT_split_3x5_3(
-      KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,     KC_CIRC, KC_AMPR, KC_ASTR, KC_PLUS, TD(EQL_GODEF),
-      KC_LCTL, KC_LT,   KC_LPRN, KC_RPRN, KC_GT,       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,
-      KC_NO,   KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR,     KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_F12,
-          KC_NO, KC_NO, DF(_ALPHA),                        DF(_SYMB),  KC_TRNS, KC_NO
+      KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, /**/ KC_CIRC, KC_AMPR, KC_ASTR, KC_PLUS, TD(EQL_GODEF),
+      KC_LCTL, KC_LT,   KC_LPRN, KC_RPRN, KC_GT,   /**/ KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, ___,
+      ___,     KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, /**/ KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_F12,
+      /**/ ___, ___, DF(_ALPHA), /*                        */ DF(_SYMB),  KC_TRNS, ___
       ),
   [_TMUX] = LAYOUT_split_3x5_3(
-      VIM_MACRO, VIM_SAVE,    KC_NO,    KC_NO,      TMUX_SP_VT,      VIM_CLIP_CP, KC_NO,      KC_NO,      KC_NO,      VIM_CLIP_PST,
-      KC_NO,     TMUX_SCROLL, KC_NO,    C(KC_TILD), KC_NO,           TMUX_PN_LT,  TMUX_PN_DN, TMUX_PN_UP, TMUX_PN_RT, TMUX_SP_HZ,
-      TMUX_ZOOM, KC_NO,       TMUX_NEW, KC_NO,      KC_NO,           TMUX_WN_LT,  KC_NO,      KC_NO,      TMUX_WN_RT, KC_NO,
-          KC_NO, KC_DEL, DF(_ALPHA),                                     DF(_TMUX), KC_TRNS, KC_NO
+      VI_MACRO, VI_SAVE,   ___,   ___,        TX_SP_VT, /**/ VI_CLIP_CP, ___,      ___,      ___,      VI_CLIP_PST,
+      ___,      TX_SCROLL, ___,   C(KC_TILD), ___,      /**/ TX_PN_LT,   TX_PN_DN, TX_PN_UP, TX_PN_RT, TX_SP_HZ,
+      TX_ZOOM,  ___,       TX_NEW, ___,       ___,      /**/ TX_WN_LT,   ___,      ___,      TX_WN_RT, ___,
+      /**/ ___, KC_DEL, DF(_ALPHA), /*                                */ DF(_TMUX), KC_TRNS, ___
       ),
 };
